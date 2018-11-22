9769957e4de47804cef863ee65aafd142c54d92f
Chintan Vijan <35058356+chintanvijan@users.noreply.github.com>
2018-11-22 19:35:00 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -4,18 +4,21 @@ import pytesseract
 import argparse
 import cv2
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,help="input_images/image1.jpg")
-args = vars(ap.parse_args())
+def func():
+  ap = argparse.ArgumentParser()
+  ap.add_argument("-i", "--image", required=True,help="input_images/image1.jpg")
+  args = vars(ap.parse_args())
 
-image = cv2.imread(args["image"])
-#cv2.imshow("Original", image)
+  image = cv2.imread(args["image"])
+  #cv2.imshow("Original", image)
 
-blurred = cv2.blur(image, (3,3))
-#cv2.imshow("Blurred_image", blurred)
-img = Image.fromarray(blurred)
-text = pytesseract.image_to_string(img, lang='eng')
-words = text.split("\n")
-print(text)
-print(words)
-#cv2.waitKey(0)
+  blurred = cv2.blur(image, (3,3))
+  #cv2.imshow("Blurred_image", blurred)
+  img = Image.fromarray(blurred)
+  text = pytesseract.image_to_string(img, lang='eng')
+  words = text.split("\n")
+  print(text)
+  print(words)
+  #cv2.waitKey(0)
+ 
+func()
