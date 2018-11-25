c671120adef8cb12218d68466001277d22de32cd
Chintan Vijan <35058356+chintanvijan@users.noreply.github.com>
2018-11-25 19:20:11 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,24 +1,4 @@
-#Extracting text from image
-from PIL import Image
-import pytesseract
-import argparse
-import cv2
-
-def func():
-  ap = argparse.ArgumentParser()
-  ap.add_argument("-i", "--image", required=True,help="input_images/image1.jpg")
-  args = vars(ap.parse_args())
-
-  image = cv2.imread(args["image"])
-  #cv2.imshow("Original", image)
-
-  blurred = cv2.blur(image, (3,3))
-  #cv2.imshow("Blurred_image", blurred)
-  img = Image.fromarray(blurred)
-  text = pytesseract.image_to_string(img, lang='eng')
-  words = text.split("\n")
-  print(text)
-  print(words)
-  #cv2.waitKey(0)
+#main file for extracting text
+from extract import ext
  
-func()
+ext.func()
